b070690b823d63547deb1e39308d21c2f375ff1c
Vijay Pushkin <a.vijaypushkin@gmail.com>
2023-08-29 12:42:42 +0530

    fix: nav fixed

--- apps/react-app/src/components/layout/Sidebar.tsx
+++ apps/react-app/src/components/layout/Sidebar.tsx
@@ -1,16 +1,15 @@
 import { Box, NavLink, Navbar } from "@mantine/core";
 import React from "react";
 import { useGetCategoriesWithChildren } from "../../graphql/queries/categories.query";
-import { Link } from "react-router-dom";
+import { Link, useLocation, useRoutes } from "react-router-dom";
 
 interface SidebarProps {
   opened: boolean;
 }
 
 const Sidebar: React.FC<SidebarProps> = (props) => {
+  const location = useLocation();
   const { data } = useGetCategoriesWithChildren();
-
-  console.log(data);
 
   return (
     <Navbar
@@ -21,17 +20,22 @@ const Sidebar: React.FC<SidebarProps> = (props) => {
     >
       <Box w={240}>
         {data?.categoriesWithChildren?.map((category) => (
-          <Link to={`/categories/${category.categoryID}`} key={category._id}>
-            <NavLink
-              key={category._id}
-              label={category.name}
-              childrenOffset={28}
-            >
-              {category.children?.map((child) => (
-                <NavLink key={child._id} label={child.name} />
-              ))}
-            </NavLink>
-          </Link>
+          <NavLink key={category._id} label={category.name} childrenOffset={28}>
+            {category.children?.map((child) => (
+              <Link
+                to={`/categories/${child.categoryID}`}
+                key={child.categoryID}
+              >
+                <NavLink
+                  key={child._id}
+                  label={child.name}
+                  active={
+                    location.pathname === `/categories/${child.categoryID}`
+                  }
+                />
+              </Link>
+            ))}
+          </NavLink>
         ))}
       </Box>
     </Navbar>
