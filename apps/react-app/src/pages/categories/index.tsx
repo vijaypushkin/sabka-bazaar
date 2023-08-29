ce55cf697333ca811a7adc245071d98e24fa6e46
Vijay Pushkin <a.vijaypushkin@gmail.com>
2023-08-29 22:18:46 +0530

    feat: categories page added

--- apps/react-app/src/pages/categories/index.tsx
+++ apps/react-app/src/pages/categories/index.tsx
@@ -1,9 +1,44 @@
+import { Box, Card, Divider, Flex, Text, Title } from "@mantine/core";
 import React from "react";
-
-interface CategoriesPageProps {}
+import { useGetCategoriesWithChildren } from "../../graphql/queries/categories.query";
 
 const CategoriesPage: React.FC = () => {
-  return <div>CategoriesPage</div>;
+  const { data } = useGetCategoriesWithChildren();
+  return (
+    <>
+      <Title order={1}>Shop By</Title>
+
+      {data?.categoriesWithChildren?.map((category) => (
+        <Box key={category._id} mb="lg">
+          <Divider
+            my="xs"
+            size="md"
+            label={
+              <Title ml={5} order={2}>
+                {category.name}
+              </Title>
+            }
+          />
+          <Flex
+            mih={50}
+            gap="md"
+            justify="flex-start"
+            align="flex-start"
+            direction="row"
+            wrap="wrap"
+          >
+            {category.children?.map((child) => (
+              <Card key={child._id} withBorder shadow="sm">
+                <Card.Section p="md">
+                  <Text size="lg">{child.name}</Text>
+                </Card.Section>
+              </Card>
+            ))}
+          </Flex>
+        </Box>
+      ))}
+    </>
+  );
 };
 
 export default CategoriesPage;
