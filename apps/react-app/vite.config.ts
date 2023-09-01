1666a5dc263661a11da59f5915fca93dab734128
Vijay Pushkin <a.vijaypushkin@gmail.com>
2023-09-01 10:52:34 +0530

    chore: added test suite

--- apps/react-app/vite.config.ts
+++ apps/react-app/vite.config.ts
@@ -1,10 +1,20 @@
-import { defineConfig } from "vite";
+import { defineConfig } from "vitest/config";
 import react from "@vitejs/plugin-react";
+// import "@testing-library/jest-dom/vitest";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     port: 3000,
+  },
+  test: {
+    environment: "jsdom",
+    setupFiles: ["./src/tests/setup.test.ts"],
+    // testMatch: ["./tests/**/*.test.tsx"],
+    globals: true,
+    coverage: {
+      provider: "v8",
+    },
   },
 });
